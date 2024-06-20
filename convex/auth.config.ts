d1037ad667500bc794c0d3b9e1a9c1c967973a9a
Norbert Kohari <106980094+KohariiDev@users.noreply.github.com>
2024-06-20 15:40:21 +0200

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://peaceful-cheetah-63.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
